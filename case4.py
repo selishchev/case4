a902f4fcce78fbb430ea44663abb8458751eede0
arkroft <44421320+arkroft@users.noreply.github.com>
2018-11-04 23:56:01 +0700

    Add files via upload

--- case4.py
+++ case4.py
@@ -3,15 +3,15 @@
 Селищев А., Паймушкин К., Кривошеенкова Е.
 
 """
-
-text = input('Введите текст:')
+import ru_local
+text = input('{}'.format(ru_local.TEXT))
 text_lower = text.lower()
 num_of_sentences = 0
 num_of_words = 1
-list_of_vowels = 'аеёиоуыюяэ'
+list_of_vowels = '{}'.format(ru_local.AE)
 num_of_syllables = 0
 for i in text_lower:
-    if i == '.':
+    if i == '.' or i == '!' or i == '?':
         num_of_sentences += 1
     if i == ' ':
         num_of_words += 1
@@ -20,17 +20,17 @@ for i in text_lower:
 ASL = num_of_words / num_of_sentences
 ASW = num_of_syllables / num_of_words
 FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
-print('Предложений:', num_of_sentences)
-print('Слов:', num_of_words)
-print('Слогов:', num_of_syllables)
-print('Средняя длина предложения в словах:', ASL)
-print('Средняя длина слова в слогах:', ASW)
-print('Индекс удобочитаемости Флеша:', FRE)
+print('{}'.format(ru_local.SUP), num_of_sentences)
+print('{}'.format(ru_local.WORD), num_of_words)
+print('{}'.format(ru_local.SYL), num_of_syllables)
+print('{}'.format(ru_local.SENT), ASL)
+print('{}'.format(ru_local.MDW), ASW)
+print('{}'.format(ru_local.IND), FRE)
 if FRE > 80:
-    print('Текст очень легко читается (для младших школьников).')
+    print('{}'.format(ru_local.EASY))
 elif 50 < FRE <= 80:
-    print('Простой текст (для школьников).')
+    print('{}'.format(ru_local.SCHOOL))
 elif 25 < FRE <= 50:
-    print('Текст немного трудно читать (для студентов).')
+    print('{}'.format(ru_local.STUD))
 elif FRE <= 25:
-    print('Текст трудно читается (для выпускников ВУЗов).')
+    print('{}'.format(ru_local.HARD))
