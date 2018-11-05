420eaa05e3d595a2e2cca01f3020e7f58dfafeed
Кирилл Паймушкин (Kirill Paymushkin) <44719713+paymtime@users.noreply.github.com>
2018-11-06 00:01:58 +0700

    Add files via upload

--- case4.py
+++ case4.py
@@ -6,23 +6,23 @@
 import ru_local
 text = input('{}'.format(ru_local.TEXT))
 text_lower = text.lower()
-num_of_sentences = 0
-num_of_words = 1
+count_sentens = 0
+count_words = 1
 list_of_vowels = '{}'.format(ru_local.AE)
-num_of_syllables = 0
+count_syllables = 0
 for i in text_lower:
     if i == '.' or i == '!' or i == '?':
-        num_of_sentences += 1
+        count_sentens += 1
     if i == ' ':
-        num_of_words += 1
+        count_words += 1
     if i in list_of_vowels:
-        num_of_syllables += 1
-ASL = num_of_words / num_of_sentences
-ASW = num_of_syllables / num_of_words
+        count_syllables += 1
+ASL = count_words / count_sentens
+ASW = count_syllables / count_words
 FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
-print('{}'.format(ru_local.SUP), num_of_sentences)
-print('{}'.format(ru_local.WORD), num_of_words)
-print('{}'.format(ru_local.SYL), num_of_syllables)
+print('{}'.format(ru_local.SUP), count_sentens)
+print('{}'.format(ru_local.WORD), count_words)
+print('{}'.format(ru_local.SYL), count_syllables)
 print('{}'.format(ru_local.SENT), ASL)
 print('{}'.format(ru_local.MDW), ASW)
 print('{}'.format(ru_local.IND), FRE)
